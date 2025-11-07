703cdf2ce10e13ce4488fb7a50350a43ab6ddf48
Bence Bertalan <bencebertalan.it@gmail.com>
2025-11-07 09:25:55 +0100

    Committing 1 item from workspace fe6e27d5-18cd-4f78-a9a4-89d87b04256e

---
+++ PT_NB_01.Notebook/notebook-content.py
@@ -0,0 +1,24 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# MARKDOWN ********************
+
+# # Welcome to your new notebook
+# # Type here in the cell editor to add code!
+
+
+# MARKDOWN ********************
+
+# fwefwefwefw
+
+# MARKDOWN ********************
+
+# fewfwefwefwefwefwef
